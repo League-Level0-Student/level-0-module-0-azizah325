5158f57809a8bef196d8f2a638287e175cf0863d
root <root@67201097ee49>
2020-06-19 00:00:08 +0000

    robot square

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -8,43 +8,42 @@ import org.jointheleague.graphical.robot.Robot;
 import java.awt.Color;
 
 public class PentagonCrazy {
-	
+
 	private void drawPattern() {
-		
-		//  Create a new Robot
 
-		//  Put the robot's pen down
+		// Create a new Robot
+		Robot rob = new Robot();
+		// Put the robot's pen down
+		rob.penDown();
+		// SPEED. Make the robot go at maximum speed (100)
+		rob.setSpeed(100);
+		// COLOR. Set the pen to a color that you like for the shape
+		rob.setPenColor(Color.red);
+		// NUMBER OF SIDES. Make an int variable for the number of sides the shape has.
+		// (Hint: its called PentagonCrazy)
+		int pentagonsides = 5;
+		// TURN ANGLE. Make another int variable for the angle you want the robot to
+		// turn.
+		// Hint: Divide 360 by the number of sides the shape has.
 
-		//  SPEED. Make the robot go at maximum speed (100)
+		// LOOP. Use a for loop to repeat ALL the following lines of code 200 times.
 
-		//  COLOR. Set the pen to a color that you like for the shape
+		// DRAW. Make the robot move "i" pixels
+		// "i" is the variable in the for loop
 
-		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape has.
-		//  				(Hint: its called PentagonCrazy)
-
-		//  TURN ANGLE. Make another int variable for the angle you want the robot to turn. 
-		//  			Hint: Divide 360 by the number of sides the shape has.
-
-		//  LOOP. Use a for loop to repeat ALL the following lines of code 200 times. 
-		
-				//  DRAW.  Make the robot move "i" pixels
-				//         "i" is the variable in the for loop
-		
-				//  TURN.  Turn the robot the amount in your angle variable + 1
-
+		// TURN. Turn the robot the amount in your angle variable + 1
 
 		// Hide the robot so you can see the pattern.
-		
-		// Check the pattern against the picture in the recipe. If it matches, you are done!
+
+		// Check the pattern against the picture in the recipe. If it matches, you are
+		// done!
 	}
 
-	
 	// Variations:
 	// *12. make the pattern really huge
-	// *13. randomize the color of the pattern 
+	// *13. randomize the color of the pattern
 	// *14. experiment with different shapes
 
-	
 	public static void main(String[] args) {
 		new PentagonCrazy().drawPattern();
 	}
