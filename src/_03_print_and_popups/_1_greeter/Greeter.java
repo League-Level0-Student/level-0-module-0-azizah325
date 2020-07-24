29386e49efbdf79ce7182ba45c2dcf7a6e4c9414
root <root@80043e5e5f22>
2020-07-24 00:01:54 +0000

    Greeter

---
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -0,0 +1,11 @@
+package _03_print_and_popups._1_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+ public static void main(String[] args) {
+	System.out.println("hello world");
+	String Input=JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null, "Hello "+Input);
+} 
+}
